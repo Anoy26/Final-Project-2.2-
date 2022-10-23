e41b5a49bd175071790f148c0f7b47db3b59846b
Md-M-R-Nahid <mdmrnahid123@gmail.com>
2022-10-23 20:56:12 +0600

    Update Adminsingup.php

--- php/Adminsingup.php
+++ php/Adminsingup.php
@@ -156,7 +156,7 @@
 
         $filename =  $_FILES["uploadfile"]["name"];
         $tempname =  $_FILES["uploadfile"]["tmp_name"];
-        $folder = "images/".$filename;
+        $folder = "../images/".$filename;
         move_uploaded_file($tempname,$folder);
 
         $name  = $_POST['name'];
