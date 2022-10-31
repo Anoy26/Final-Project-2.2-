4d466d66d6712922cde6f4aea0a18dbe5a18e865
Md-M-R-Nahid <mdmrnahid123@gmail.com>
2022-10-31 22:43:01 +0600

    Update admitcard.php

--- php/admitcard.php
+++ php/admitcard.php
@@ -6,6 +6,12 @@
     $data = mysqli_query($conn, $query2);
 
     $result = mysqli_fetch_assoc($data);
+
+    session_start();
+    $user2=$_SESSION['user_name'];
+    $query2="SELECT * FROM student_info WHERE User_ID='$user2'";
+    $data2=mysqli_query($conn, $query2);
+    $result2=mysqli_fetch_assoc($data2);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,9 +24,19 @@
 <body>
     <div>
     <h1 align="center"> Admit Card  </h1>
+    <h2 align="center">Exam Roll:<?php echo $user; ?><h2>
     </div>
     <div>
-      <center> <img src='<?php  echo $result[File2]; ?>' height='240px' width='240px'></center>
+      <center> 
+        <img src='<?php echo $result2[Stu_img]; ?>' height='240px' width='240px'>
+        <img src='<?php  echo $result[File1]; ?>' height='240px' width='240px'>
+      </center>
+    </div>
+    <h2 align="center">Scan the QR code for attendence<h2>
+    <div>
+      <center>
+        <img src='<?php echo $result[File2]; ?>' height='240px' width='240px'>
+      </center>
     </div>
 </body>
 </html>
